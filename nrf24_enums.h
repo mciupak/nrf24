a9f800a16653769e48c9fda3ab8a160b818a08e3
Marcin Ciupak <marcin.s.ciupak@gmail.com>
2017-10-12 12:22:50 +0200

    checkpatch.pl fixes.
    Signed-off-by: Marcin Ciupak <marcin.s.ciupak@gmail.com>

--- nrf24_enums.h
+++ nrf24_enums.h
@@ -20,51 +20,51 @@
 #define NRF24_ENUMS_H
 
 enum nrf24_pipe_num {
-        NRF24_PIPE0,
-        NRF24_PIPE1,
-        NRF24_PIPE2,
-        NRF24_PIPE3,
-        NRF24_PIPE4,
-        NRF24_PIPE5,
-        NRF24_TX,
-        NRF24_PIPE_ALL = 0xFF
+	NRF24_PIPE0,
+	NRF24_PIPE1,
+	NRF24_PIPE2,
+	NRF24_PIPE3,
+	NRF24_PIPE4,
+	NRF24_PIPE5,
+	NRF24_TX,
+	NRF24_PIPE_ALL = 0xFF
 };
 
 enum nrf24_crc_mode {
-        NRF24_CRC_OFF,
-        NRF24_CRC_8BIT = 2,
-        NRF24_CRC_16BIT
+	NRF24_CRC_OFF,
+	NRF24_CRC_8BIT = 2,
+	NRF24_CRC_16BIT
 };
 
 enum nrf24_address_width {
-        NRF24_AW_3 = 3,
-        NRF24_AW_4,
-        NRF24_AW_5
+	NRF24_AW_3 = 3,
+	NRF24_AW_4,
+	NRF24_AW_5
 };
 
 enum nrf24_pload {
-        NRF24_TX_PLOAD = 7,
-        NRF24_TX_PLOAD_NOACK,
-        NRF24_RX_PLOAD,
-        NRF24_ACK_PLOAD
+	NRF24_TX_PLOAD = 7,
+	NRF24_TX_PLOAD_NOACK,
+	NRF24_RX_PLOAD,
+	NRF24_ACK_PLOAD
 };
 
 enum nrf24_datarate {
-        NRF24_DATARATE_1MBPS,
-        NRF24_DATARATE_2MBPS,
-        NRF24_DATARATE_256KBPS
+	NRF24_DATARATE_1MBPS,
+	NRF24_DATARATE_2MBPS,
+	NRF24_DATARATE_256KBPS
 };
 
 enum nrf24_mode {
-        NRF24_MODE_TX,
-        NRF24_MODE_RX
+	NRF24_MODE_TX,
+	NRF24_MODE_RX
 };
 
 enum nrf24_rf_power {
-        NRF24_POWER_18DBM,
-        NRF24_POWER_12DBM,
-        NRF24_POWER_6DBM,
-        NRF24_POWER_0DBM
+	NRF24_POWER_18DBM,
+	NRF24_POWER_12DBM,
+	NRF24_POWER_6DBM,
+	NRF24_POWER_0DBM
 };
 
 #endif
