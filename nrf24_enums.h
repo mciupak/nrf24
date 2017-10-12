01b3ce23e286bb73ef825cb87d747837b303bc21
Marcin Ciupak <marcin.s.ciupak@gmail.com>
2017-10-12 12:17:04 +0200

    Type fix in email address.
    Signed-off-by: Marcin Ciupak <marcin.s.ciupak@gmail.com>

--- nrf24_enums.h
+++ nrf24_enums.h
@@ -1,6 +1,6 @@
 /*
  * nRF24L01 device driver.
- * Copyright (C) 2017 Marcin Ciupak <marcin.s.ciupak@gmail.cm>
+ * Copyright (C) 2017 Marcin Ciupak <marcin.s.ciupak@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
